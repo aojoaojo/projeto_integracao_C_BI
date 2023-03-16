2fd6adc5e1e2107f12133aad9a78d4715635c08b
aojoaojo <aojoaojov@gmail.com>
2023-03-16 10:42:57 -0300

    Este commit arrumou os erros na lista, agora eh possivel adicionar os itens a lista e imprimir os valores

--- programa_gestao_financeira.c
+++ programa_gestao_financeira.c
@@ -12,50 +12,95 @@ typedef struct tipo_geral_node
 typedef struct tipo_geral_head
 {
     geral_node *primeiro;
+
 } geral_head;
 
-int menu()
+void criar_node_geral(geral_head *head, char string[50]);
+
+void mostrar_lista(geral_head *cabeca)
 {
-    int opt;
+    geral_node *atual;
+    atual = cabeca->primeiro;
+    if (cabeca->primeiro == NULL)
+    {
+        printf("Cabeça está vazia\n");
+        return;
+    }
+    while (atual != NULL)
+    {
+        printf("%s\n", atual->class_geral);
+        atual = atual->proximo;
+    }
+}
+
+void criar_node_geral(geral_head *head, char string[50])
+{
+    geral_node *novo;
+    geral_node *atual = head->primeiro;
+    novo = (geral_node *)malloc(sizeof(geral_node));
+    strcpy(novo->class_geral, string);
+    if (head->primeiro == NULL)
+    {
+        head->primeiro = novo;
+        return;
+    }
+    while (1)
+    {
+        if (atual->proximo == NULL)
+        {
+            atual->proximo = novo;
+            novo->proximo = NULL;
+            printf("okay\n");
+            break;
+        }
+
+        atual = atual->proximo;
+    }
+}
+
+int menu(int *opt, geral_head *head)
+{
+
     printf("Escolha uma opção:\n");
     printf("1 - Inserir gasto\n2 - Remover gasto\n3 - Mostrar gastos\n4 - Sair\n");
-    scanf("%d", &opt);
-    if (opt == 1)
+    scanf("%d", opt);
+    if (*opt == 1)
     {
-        printf("1\n");
+        char string[50];
+        printf("Insira uma string\n");
+        // fgets(string);
+        scanf("%s", string);
+        criar_node_geral(head, string);
     }
-    else if (opt == 2)
+    else if (*opt == 2)
     {
         printf("2\n");
     }
-    else if (opt == 3)
+    else if (*opt == 3)
     {
-        printf("3\n");
+        mostrar_lista(head);
     }
-    else if (opt == 4)
+    else if (*opt == 4)
     {
         printf("Volte sempre!\n");
     }
     else
     {
         printf("Opção inválida\n");
+        return 4;
     }
 
-    return opt;
-}
-
-void criar_node_geral(geral_head* head, char string[50]){
-    geral_node *novo;
-    novo = (geral_node*)malloc(sizeof(geral_node));
-    novo->proximo = NULL;
-    strcpy(novo->class_geral, string);
+    return *opt;
 }
 
 int main(int argc, char const *argv[])
 {
+    geral_head *head = (geral_head *)malloc(sizeof(geral_head));
+    head->primeiro = NULL;
+    int opcao_1;
     while (1)
     {
-        int opcao_1 = menu();
+        menu(&opcao_1, head);
         if (opcao_1 == 4)
             break;
     }
